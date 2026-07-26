3574f1996a1ee19155357d6e206f743251f1599b
Simone Rizzo <35865705+SimonTek27@users.noreply.github.com>
2026-07-26 20:10:26 +0200

    Delete build_configure.bat

--- build_configure.bat
+++
@@ -1,5 +0,0 @@
-cd E:\Users\Simon\source\repos\kseditor
-rmdir /s /q build 2>nul
-mkdir build
-cd build
-cmake -G "Visual Studio 18 2026" -A x64 -DCMAKE_PREFIX_PATH="C:/Qt/6.11.1/msvc2022_64" -S .. -B .
