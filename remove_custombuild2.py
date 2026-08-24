4a51c3a7352fc56e0a599dc8624f299449ac3a4e
Simone Rizzo <35865705+SimonTek27@users.noreply.github.com>
2026-08-24 15:18:13 +0200

    Delete remove_custombuild2.py

--- remove_custombuild2.py
+++
@@ -1,8 +0,0 @@
-import re
-with open(r'E:\Users\Simon\source\repos\kseditor\ZERO_CHECK.vcxproj', 'r') as f:
-    content = f.read()
-# Remove all CustomBuild blocks
-content = re.sub(r'<CustomBuild Include="E:.*?">.*?</CustomBuild>', '', content, flags=re.DOTALL)
-with open(r'E:\Users\Simon\source\repos\kseditor\ZERO_CHECK.vcxproj', 'w') as f:
-    f.write(content)
-print('Done')
